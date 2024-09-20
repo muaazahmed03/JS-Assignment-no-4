ac0eb3c3af9f96db70830f169120513c21a07788
Muaaz Ahmed Baig <muaazahmed0111@gmail.com>
2024-09-20 15:45:47 +0500

    JS Assignment no 4

---
+++ PDF.js
@@ -0,0 +1,226 @@
+// Chapter 10 (Word File)
+
+// Q1 
+// var city = prompt("What's your country??")
+
+// if(city === "karachi"){
+//     console.log("The city of lights")
+// }
+// else{
+//     console.log("Welcome!")
+// }
+
+// Q2 
+// var x = "y"
+// if(x === "y"){
+//     prompt("What Is The Value Of Z")
+// }
+
+// Q3 
+// var zipCode = +prompt("Tell me your city ZipCode")
+
+// if(zipCode === 10010){
+//     alert("Karachi")
+// }
+// else{
+//     alert("Please Write Correct City")
+// }
+
+// Q4 
+// var x = 55
+// if(x === 55){
+//     x = 1
+// }
+// alert(x)
+
+// Chapter 11 (Comparison Operators) 
+
+// Q1 
+// var a = b 
+// if(a !== b){
+//     alert("OK")
+// }
+
+// Q2 
+// var num1 = 55
+// var num2 = 20 
+
+// if(num1 >= num2){
+//     alert("True")
+// }
+
+// Q3 
+// var y = 2
+// if(y !== 10){
+//     y=4
+// }
+// alert(y)
+
+// Q4 
+// var num = 23
+
+// if(num !== 15){
+//     alert("OK")
+// }
+
+// Q5 
+// var name = prompt("Tell me your First name")
+
+// if(name !== "muaaz"){
+//     alert("NO MATCH")
+// }
+
+// Chapter 12 (if  else and else if statement)
+
+// Q1 
+// var num1 = +prompt("Write First Number")
+// var num2 = +prompt("Write Second Number")
+
+// if(num1 > num2){
+//     alert("The First Number is greater than Second Number")
+// }
+// else if (num1 < num2){
+//     alert("The First Number is less than Second Number")
+// }
+// else {
+//     alert("The First Number is Equal to the Second Number")
+// }
+
+// Q2 
+// var sub1 = +prompt("Enter your Marks of Subject 1")
+// var sub2 = +prompt("Enter your Marks of Subject 2")
+// var sub3 = +prompt("Enter your Marks of Subject 3")
+// var total_marks = +prompt("Enter your Total marks")
+
+// var obtained_marks = sub1 + sub2 + sub3
+// var percentage = obtained_marks/total_marks * 100
+
+// if(percentage >= 80){
+//     document.write(`The Percentage is ${percentage}%. <br> Grade A+`)
+// }
+// else if(percentage >= 70){
+//     document.write(`The Percentage is ${percentage}%. <br> Grade A`)
+// }
+// else if(percentage >= 60){
+//     document.write(`The Percentage is ${percentage}%. <br> Grade B`)
+// }
+// else if(percentage >= 50){
+//     document.write(`The Percentage is ${percentage}%. <br> Grade C`)
+// }
+// else if(percentage < 50){
+//     document.write(`The Percentage is ${percentage}%. <br> Grade D(FAIL)`)
+// }
+
+// Q3 
+// var a = 10
+// if (a === 10) {
+//     alert("a is 10");
+// } 
+// else {
+//     alert("The correct value of a is " + a);
+// }
+
+// Q4 
+// var city = prompt("Enter Your City Name")
+
+// if(city === "karachi"){
+//     alert("Acknowledging it is Karachi.")
+// }
+// else if (city === "lahore"){
+//     alert("Acknowledging it is Lahore.")
+// }
+// else {
+//     alert("Welcome!")
+// }
+
+// Chapter 13 (Testing sets of conditions)
+
+// Q1  
+// var a
+// var b
+// var c
+// var d
+// if(a===b && c===d){
+//     alert(true)
+// }
+
+// Q2 
+// var a
+// var b
+// var c
+// var d
+// if(a===b || c!==d){
+//     alert(true)
+// }
+
+// Q3 
+// var name = prompt("Ennter Your Name")
+// var age = +prompt("Enter Your Age")
+
+// if(name === "Hamza" || name === "Arsalan" || age <=60){
+//     alert(true)
+// }
+
+// Q4 
+// var a = +prompt("number 1 ??")
+// var b = +prompt("number 2 ??")
+
+// if(a < b){
+//     alert("a is less than b")
+// }
+// else if(a > b){
+//     alert("a is greater than b")
+// }
+
+// Q5 
+// var firstName = "Muaaz"
+// var lastName = "Ahmed"
+
+// var name1 = prompt("Enter your first name")
+// var name2 = prompt("Enter your last name")
+
+// if(name1 === firstName && name2 === lastName){
+//     alert("Matched")
+// }
+// else{
+//     alert("Please Enter The Correct Name")
+// }
+
+// Chapter 14 (If statements nested) 
+
+// Q1
+// var password = +prompt("Enter Password")
+// if(password <= 5){
+//     alert("Password Must Be greater than 5")
+// }
+// else{
+//     alert("Ok")
+// }
+
+// Q2
+// var a = 1
+// var c = "Max"
+// if (a === 1) {
+//     if (c === "Max") {
+//         alert("OK");
+//     }
+// }
+
+// Q3
+// var a = 1
+// var c = "Max"
+// if(a === 1 && c === "Max"){
+//     alert("Ok")
+// }
+
+// Q4
+// var a = 10
+// var b = 10
+// if(a===b){
+//     if(a <= b){
+//         alert("True")
+//     }
+// }
+
+
+//                                                     THE END 
